dbf38de4954763a32398d80f1ce1628c1c4d895a
New Wave <mustaphaadegboyega801@gmail.com>
2026-05-01 21:25:29 +0100

    Update portal.js

--- portal.js
+++ portal.js
@@ -1,4 +1,4 @@
-const scriptURL ='https://script.google.com/macros/s/AKfycbwsoYQs5tgm2m6_dbN5pQqZ6aDZzfLMdAZlf2czfzJQZT_kM-jfepZRkRTCtCW7l6c1/exec ';
+const scriptURL ='https://script.google.com/macros/s/AKfycbw8uwftZRSrZxDEUbnpS2d9MiiYlYTETnbUsz67QzQe5sopz8I8gCS2QFtyDItQtW51/exec ';
 const dropDate = new Date("2026-07-01T00:00:00").getTime();
 
 const interval = setInterval(function () {
